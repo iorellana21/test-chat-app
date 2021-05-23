c1d6b4bf937b1a6654b341ac9d10365fb450850a
Ivan Orellana Jr <iorellana21@gmail.com>
2021-05-23 14:51:14 -0400

    add our login page

--- src/components/Login/Login_2.js
+++ src/components/Login/Login_2.js
@@ -39,6 +39,56 @@ export default function Login({ onIdSubmit }) {
                 <Button onClick={createNewId} variant="secondary">Create Username</Button>
             </Form>
 
+            {/* OUR STUFF */}
+            <div className="col-6 mx-auto my-auto">
+                <div className="p-2 mx-auto d-block text-center">
+
+                    {/* TITLE */}
+                    <h2>Welcome to the Friends/Connect App</h2>
+
+                    {/* Login Form */}
+                    <form onSubmit={handleSubmit}>
+                        {/* EMAIL */}
+                        <div className="form-group">
+                            <label>Email </label>
+                            <input type="email" className="form-control" />
+                            <small className="email-help-text form-text text-muted">
+                                We'll never share your email with anyone else.
+                                </small>
+                        </div>
+
+                        {/* PASSWORD */}
+                        <div className="form-group">
+                            <label >Password</label>
+                            <input type="password" className="form-control" />
+                        </div>
+
+                        {/* USERNAME PAGE */}
+                        <Link
+                            to="/username"
+                            className={
+                                location.pathname === "/username"
+                                    ? "nav-link"
+                                    : "nav-link text-black btn btn-primary my-3 w-100"
+                            }>
+                            LOGIN
+                        </Link>
+
+                        {/* SIGNUP PAGE */}
+                        <Link
+                            to="/signup"
+                            className={
+                                location.pathname === "/signup"
+                                    ? "nav-link"
+                                    : "nav-link text-black btn btn-light my-3 w-100"
+                            }>
+                            SIGN UP
+                        </Link>
+
+
+                    </form>
+                </div>
+            </div>
 
 
             
