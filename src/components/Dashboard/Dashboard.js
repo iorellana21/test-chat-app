a015007cbae15953337f49ca94b02eaf78816747
Ivan Orellana Jr <iorellana21@gmail.com>
2021-05-23 15:04:04 -0400

    fix sidebard styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ export default function Dashboard({ id }) {
     const { selectedConversation } = useConversations()
 
     return (
-        <div className="d-flex" stye={{ height: '100vh' }}>
+        <div className="d-flex" style={{ height: '100vh' }}>
             <Sidebar id={id} />
             {selectedConversation && <OpenConversation />}
         </div>
