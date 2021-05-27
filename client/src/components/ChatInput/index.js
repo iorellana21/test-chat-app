69ac7835ec00af049a6eb62581c396a1809046cb
malhill <malcolmhc@msn.com>
2021-05-27 11:38:03 -0400

    merging branches

--- client/src/components/ChatInput/index.js
+++ client/src/components/ChatInput/index.js
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaHome } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
-import HandleChat from '../../HandleChat';
 import './index.scss';
+
+
 
 export default function ChatInput() {
   const location = useLocation();
-
-  const { messages, sendMessage } = HandleChat("current-room");
-  const [newMessage, setNewMessage] = useState("");
-
-  const handleNewMessageChange = (event) => {
-    setNewMessage(event.target.value);
-  };
-  const handleSendMessage = () => {
-    sendMessage(newMessage);
-    setNewMessage("");
-  };
-
-  console.log(messages);
 
   return (
     <div className="container">
@@ -31,30 +19,13 @@ export default function ChatInput() {
               </Link>
             </div>
             <div className="card-body" id="msgs">
-              {messages.map((message, i) => (
-                <li
-                  key={i}
-                  className={`message-item text-dark ${message.ownedByCurrentUser ? "my-message" : "received-message"
-                    }`}
-                >
-                  {message.body}
-                </li>
-              ))}
+              <div className="msg to">Hello! How can I assist you today?</div>
             </div>
             <div className="card-footer">
               <div className="input-group" id="msgForm" data-sender="me">
-
-                <textarea
-                  value={newMessage}
-                  onChange={handleNewMessageChange}
-                  placeholder="Write message..."
-                  className="new-message-input-field"
-                />
-
+                <input className="form-control" type="text" placeholder="Type message and hit [Enter] to send." />
                 <div className="input-group-append">
-                  <button onClick={handleSendMessage} className="send-message-button">
-                    Send
-                  </button>
+                  <button className="btn btn-outline-secondary" type="button">Send</button>
                 </div>
               </div>
             </div>
