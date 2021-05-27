c723ba5e63639efe0810657199ac40043ccd386e
malhill <malcolmhc@msn.com>
2021-05-27 11:49:02 -0400

    fixed const in room and message

--- models/message.js
+++ models/message.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const messageSchema = new Schema ({
+const messageSchema = new Schema({
     room: [{
         type: Schema.Types.ObjectId,
-        ref:'Room',
+        ref: 'Room',
     }],
-    user:[{
+    user: [{
         type: Schema.Types.ObjectId,
         ref: 'User',
     }],
     message_body: String,
-    message_status:{type: Boolean, default: false},
+    message_status: { type: Boolean, default: false },
     created_at: { type: Date, default: Date.now },
 })
-const Message = new Message = mongoose.model("Message", messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
 
 
