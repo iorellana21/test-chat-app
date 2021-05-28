0ea52c24fdd3d98b0fdb546d2620a8f3497e4dfd
malhill <malcolmhc@msn.com>
2021-05-27 21:24:52 -0400

    deleted build folder, file duplicates as well

--- routes/api/user 2.js
+++ routes/api/user 2.js
@@ -8,7 +8,6 @@ router.get("/test", (req, res) => {
     res.json(true);
 });
 
-
 // Using the passport.authenticate middleware with our local strategy.
 // If the user has valid login credentials, send them to the members page.
 // Otherwise the user will be sent an error
